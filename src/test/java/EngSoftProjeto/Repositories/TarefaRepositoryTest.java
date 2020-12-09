d95af06bda18bcd78b5edea4c2b964601af8fe38
JoaoFortunato14 <71406370+JoaoFortunato14@users.noreply.github.com>
2020-12-09 16:30:33 +0000

    Repositorios

---
+++ src/test/java/EngSoftProjeto/Repositories/TarefaRepositoryTest.java
@@ -0,0 +1,50 @@
+package EngSoftProjeto.Repositories;
+
+import EngSoftProjeto.Models.Cargo;
+import EngSoftProjeto.Models.Empregado;
+import EngSoftProjeto.Models.Tarefa;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class TarefaRepositoryTest {
+
+
+    @Autowired
+    private TarefaRepository tarefaRepository;
+
+    @Autowired
+    private EmpregadoRepository empregadoRepository;
+
+    @Test
+    public void testeCriarTarefa(){
+
+        //ERRO
+        Tarefa tf = new Tarefa();
+        Empregado emp1 = new Empregado();
+
+        emp1.setId(1L);
+        emp1.setNome("Andre B");
+        emp1.setCargo(Cargo.DesenvolvedorJr);
+        emp1.setHoraValor(10);
+
+        tf.setId(1L);
+        tf.setEmpregado(emp1);
+        tf.setDuracao(60);
+
+        emp1.addTarefa(tf);
+
+        assertEquals(0, tarefaRepository.count());
+        assertEquals(0, empregadoRepository.count());
+
+        tarefaRepository.save(tf);
+        empregadoRepository.save(emp1);
+
+        assertEquals(1, tarefaRepository.count());
+        assertEquals(1, empregadoRepository.count());
+
+    }
+}
